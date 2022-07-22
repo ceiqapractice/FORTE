04dc9dbbe1d21e6efcf9c08265b2e68956dc6b32
Ganeshcei <dganesh@ceiamerica.com>
2022-07-22 12:41:14 +0530

    Playwright changes

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,7 +3,8 @@ const { log } = require("util");
 const { LoginPage } = require("../pageObjects/pageFunctions/LoginPage");
 const { DataHandler } = require("../testData/Datahandler");
 const { webActions } = require("../webActions/webActions");
-//Login test
+
+
 test("Login to the application", async ({page})=>
   {
     const webActionObj =new webActions(page);
