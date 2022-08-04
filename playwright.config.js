44a72d5db4f480c8947cbd3accca35339bb9ba10
Ganeshcei <dganesh@ceiamerica.com>
2022-08-04 13:50:29 +0530

    docker changes

--- playwright.config.js
+++ playwright.config.js
@@ -9,11 +9,12 @@ const config = {
     timeout: 5000,
     toMatchSnapshot:{threshold:0.05}
   },
-  reporter: [ ['html', { outputFolder: './playwright_report/'+ ReportDate}]],
+  //reporter: [ ['html', { outputFolder: './playwright_report/'+ ReportDate}]],
+  reporter: [['experimental-allure-playwright']],
   projects: [
     {   
       use: {
-        browserName:"chromium",
+        browserName:"firefox",
         headless:true,
       },
   
--- webTest/getAllElemetns.spec.js
+++ webTest/getAllElemetns.spec.js
@@ -1,51 +1,22 @@
 // import { xPath } from "playwright-dompath";
 
 const { test, selectors } = require("@playwright/test");
-const { xPath } = require("playwright-dompath")
+const { xPath } = require("playwright-dompath");
+const { timeout } = require("../playwright.config");
 const domToPlaywright = require('dom-to-playwright').default;
 
 
+
 test("@allelements", async ({ page }) => {
-   //await page.goto("https://parabank.parasoft.com/parabank/index.htm");
-   //await page.goto("https://opensource-demo.orangehrmlive.com/index.php/dashboard");
-   await page.goto("https://www.udemy.com/");
+
+  //await page.goto("https://parabank.parasoft.com/parabank/index.htm");
+  await page.goto("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+  //await page.goto('https://letcode.in/elements');
   
-   await page.waitForSelector('*')
-//   const inputs = await page.$$("input");
-//   console.log(inputs.length);
-//   for(let input of inputs) {
-//     let res = await xPath(input);
-//     console.log(res);
-//   }
-//   // inputs.forEach(input => xPath(input).then(res => console.log(res)))
-// })
-await page.waitForSelector("*",{timeout:15000})
-const allelements=await page.$$("*");
-
-const allelementsInnerHTML=await Promise.all(allelements.map(async(repo,i)=>{
-  return await repo.innerHTML();
-
-}))
-var ele=[];
-let canCollect=false;
-let countInput=0;
-for(var elem of allelementsInnerHTML)
-{
-  if(elem.startsWith("<" + "input"))
-  {
-    canCollect=true;
-    countInput=countInput+1
-  }
-  if(canCollect)
-  {
-    if (elem.startsWith("<" + "input") || elem.endsWith("</" + "input"))
-    ele.push(elem)
-  }
-  if (elem.startsWith("</" + "input")) {
-    canCollect = false ;
-}
-}
-console.log(countInput)
-console.log(ele);
-console.log(ele.length);
-})
+  await page.waitForSelector("form", { timeout: 5000 })
+        const repos = await page.$$("form");
+        const allUrls = await Promise.all(repos.map(async (repo, i) => {
+            return await repo.innerHTML()
+        }))
+        console.log(allUrls);
+      })
