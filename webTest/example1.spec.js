f48dbcca46999f84e661ff44b3fd3c4cf207add4
Ganeshcei <dganesh@ceiamerica.com>
2022-08-04 21:35:35 +0530

    allure change 12

--- webTest/example1.spec.js
+++ webTest/example1.spec.js
@@ -19,7 +19,7 @@ test("@Regression", async ({page})=>
   {
     const webActionObj =new webActions(page);
     const loginPageObj=new LoginPage(page);
-    console.log("Smoke test allure01 001");
+    console.log("Smoke test allure report1");
     await webActionObj.navigateToURL();
     console.log("Page navigation");
     await loginPageObj.LoginToApps();
