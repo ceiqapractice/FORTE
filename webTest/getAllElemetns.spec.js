fa24681dae1e8b6b56f7ef2a6b3e955510c851e7
Ganeshcei <dganesh@ceiamerica.com>
2022-08-04 18:49:19 +0530

    yml changes

--- webTest/getAllElemetns.spec.js
+++ webTest/getAllElemetns.spec.js
@@ -9,14 +9,14 @@ const domToPlaywright = require('dom-to-playwright').default;
 
 test("@allelements", async ({ page }) => {
 
-  //await page.goto("https://parabank.parasoft.com/parabank/index.htm");
-  await page.goto("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+  await page.goto("https://parabank.parasoft.com/parabank/index.htm");
+  //await page.goto("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
   //await page.goto('https://letcode.in/elements');
   
   await page.waitForSelector("form", { timeout: 5000 })
-        const repos = await page.$$("form");
-        const allUrls = await Promise.all(repos.map(async (repo, i) => {
-            return await repo.innerHTML()
-        }))
-        console.log(allUrls);
+        const repos = await page.content();
+        // const allUrls = await Promise.all(repos.map(async (repo, i) => {
+        //     return await repo.innerHTML() 
+        //    }))
+        console.log(repos);
       })
