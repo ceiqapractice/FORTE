1aabd2fa788ed350672bd6a46ca456c53a67ae5c
Ganeshcei <dganesh@ceiamerica.com>
2022-08-05 14:12:46 +0530

    allure report generation

--- webTest/getAllElemetns.spec.js
+++ webTest/getAllElemetns.spec.js
@@ -9,14 +9,24 @@ const domToPlaywright = require('dom-to-playwright').default;
 
 test("@allelements", async ({ page }) => {
 
-  await page.goto("https://parabank.parasoft.com/parabank/index.htm");
+  //await page.goto("https://parabank.parasoft.com/parabank/index.htm");
   //await page.goto("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
-  //await page.goto('https://letcode.in/elements');
-  
-  await page.waitForSelector("form", { timeout: 5000 })
-        const repos = await page.content();
-        // const allUrls = await Promise.all(repos.map(async (repo, i) => {
-        //     return await repo.innerHTML() 
-        //    }))
-        console.log(repos);
-      })
+  await page.goto("https://www.orangehrm.com/");
+ 
+  const inputs = await page.$$('form')
+  // console.log(typeof inputs);
+  // console.log('*********');
+   for(var p of inputs)
+   {
+    console.log(await p.getAttribute('id'));
+   }
+  // console.log('*********');
+  // console.log("Hi");
+
+  // await page.waitForSelector("form", { timeout: 5000 })
+  // const repos = await page.$("form")
+  // // const allUrls = await Promise.all(repos.map(async (repo, i) => {
+  // //     return await repo.innerHTML() 
+  // //    }))
+  // console.log(await repos.innerHTML());
+})
