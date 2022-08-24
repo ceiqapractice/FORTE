2d293aeff9204832d3b490c1103a6044dd183e5c
Ganeshcei <dganesh@ceiamerica.com>
2022-08-24 19:21:47 +0530

    Web Action changes

--- webTest/Visualcomparison.spec.js
+++ webTest/Visualcomparison.spec.js
@@ -1,66 +1,61 @@
+const fs =require("fs");
+const path=require("path")
 const {test,expect} = require('@playwright/test');
-const { testConfig } = require('../TestData/webTestData/testData');
+
+
 
 let links=[];
-let baseURL=testConfig.baseURL;
+//const foldertobetested="08102022";
+//let baseURL="https://www.ceiamerica.com/"
 
 async function comparepage(filepath,page){
     try{
-        await expect(await page.screenshot()).toMatchSnapshot([filepath]);
+        await expect(await page.screenshot({fullPage:true})).toMatchSnapshot([filepath]);
     }
-    catch(err)
-    {
-        console.log(err);
+    catch(err){
+      console.log(err);
     }}
 
-    const sleep = (milliseconds) => {
-        return new Promise(resolve => setTimeout(resolve, milliseconds))
-    }
-
-test.skip("@visual",async ({ page }) => {  
-    const  link = await import("linkinator");
+test('Visual comparison of all pages',async ({ page }) => {  
+    const link = await import("linkinator");
     const results = await link.check({
       path: baseURL,
-      linksToSkip:testConfig.linksToSkip
-       });
+      //:["https://www.facebook.com","https://twitter.com","https://www.linkedin.com","https://www.ceiamerica.com/wp","https://share.hsforms.com/","https://js.hs-scripts.com/","https://fonts.googleapis.com/","https://www.googletagmanager.com/"]
+    });
 await results
-console.log(results.links)
-  let array=results.links
-  for(let i=0;i<array.length;i++){  
-  let jsonobj=array[i];
-  if(jsonobj.state=="OK" && jsonobj.status==200){
-    console.log("pushing the url : " +jsonobj.url)
+  let array1=results.links
+  for(let i=0;i<array1.length;i++){  
+  let jsonobj=array1[i];
+  if(jsonobj.state=="OK" && jsonobj.status==200  && !(jsonobj.url.endsWith(".png") || jsonobj.url.endsWith(".svg") )){
     links.push(jsonobj.url)
   }  
   }
-  console.log(links)
-  console.log(links.length)
-      let uniqueurls=[...new Set(links)]
-      console.log(uniqueurls);
-      console.log("after promise");
-      let Screenshotpath2="VisualBaseScreenshots/";
-      let length=baseURL.length
-      try{
+  let uniqueurls=[...new Set(links)];
+  let length1=baseURL.length;
+  try{
       for(let i=0 ;i<uniqueurls.length;i++){
-      let str=uniqueurls[i];
-      console.log(str);
-    let filename=str.substring(length).replace(/[^a-zA-Z0-9]/g,'');
-    if(filename=="")
-    {
-    filename=str.substring(8).replace(/[^a-zA-Z0-9]/g,'');  
-    }
-      console.log("Filename is :" +filename)
-      let filepath2=Screenshotpath2+filename+".png";  
-      console.log("Filepath is : " +filepath2) 
+      let str=uniqueurls[i];          
       await page.goto(str);
       await page.waitForLoadState('networkidle');
-      await sleep(5000);
       await page.waitForLoadState('domcontentloaded');
-      await comparepage(filepath2,page); 
-    } } 
-    catch(err){
+      let filename1=str.substring(length1).replace(/[^a-zA-Z0-9]/g,'');
+      if(filename1=="")
+      {
+        filename1=str.substring(8).replace(/[^a-zA-Z0-9]/g,'');  
+      }
+      let filename2=filename1+"-win32.png";
+      let screenshtplace="Visualcomparisonscreenshots/"+foldertobetested+"/"+filename2;
+      let screenshotdest="tests/visualcomparison.spec.js-snapshots/"+filename2;
+      if(fs.existsSync(path.join(process.cwd(),screenshtplace))){
+        await fs.copyFileSync(screenshtplace,screenshotdest);
+        await comparepage(filename1+".png",page);
+      }
+      else{        
+      await page.screenshot({path:path.join(process.cwd(),screenshtplace),fullPage:true});
+      }
+    }}
+  catch(err){
         console.log(err);
-    }   
-  });
+  }});
 
 
