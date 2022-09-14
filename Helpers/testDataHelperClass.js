13800525093f1463c3c34b2a9dcb6ffcef835ec8
Ganeshcei <dganesh@ceiamerica.com>
2022-09-14 13:31:01 +0530

    API Changes

--- Helpers/testDataHelperClass.js
+++ Helpers/testDataHelperClass.js
@@ -15,7 +15,7 @@ loadobject(filepath){
         console.log("is undefined");
 
         const defaultobject=JSON.parse(defaultrawdata);
-        console.log("default object --> " + defaultobject[0]);   
+        console.log("default object --> " + defaultobject);   
         return defaultobject; 
         }
         else
