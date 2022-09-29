d55a4bd0c3c462ae2a0b6d293026989389b82bdf
Kanimozhi Sankar <skanimozhi@ceiamerica.com>
2022-09-29 20:54:09 +0530

    Updation of ElementFinder

--- Helpers/elementFinder.js
+++ Helpers/elementFinder.js
@@ -2,39 +2,122 @@ const {test}=require("@playwright/test")
 let testdatapath="./locators/"
 const fs=require("fs")
 const { testConfig } = require("../baseConfig")
-let id="id="
-let name1="name="
-let src="src="
-let dataobj={}
-let tagstocheck=["input","form","div"]
 
-test('@FindElements',async ({ page }) => {
+
+    let att = {
+        "a": [
+            "id",
+            "name",
+            "class",
+            "href",
+            "normalize-space(descendant-or-self::text())"],
+        "img": ["id", "class","name", "src", "alt"],
+        "input": ["id", "class","name", "value", "src"],
+        "button": [
+                "id",
+                "name",
+                "value",
+                "class",
+                "normalize-space(descendant-or-self::text())",
+            ],
+    }
+
+test('@FindElements',async({ page }) => {
     await page.goto(testConfig.elementFinderURL);
-    let pagetitle=(await page.title()).toString().replace(/\s/g,"").replace(/[^a-zA-Z0-9]/g,'');
-    for(let j=0;j<tagstocheck.length;j++){
-    const links=await page.$$(tagstocheck[j])
-    let filepath=testdatapath+pagetitle+"page.json"
-    if(fs.existsSync(filepath)){
-        let data=fs.readFileSync(filepath);
-        dataobj=JSON.parse(data);
+    let pagetitle=(await page.title()).toString().replace(/\s/g,"").replace(/[^a-zA-Z0-9]/g,''); 
+    let elementsJson = {};
+    for (let key in att){
+        //loop through each tag in att and locate all the elements
+        let attributes = att[key];
+        let count = await page.locator(key).count();
+        
+        for (let i=0;i<count;i++){
+            //loop through each of the found element locator to fetch required attributes
+            const element = await page.locator(key).nth(i);
+
+            //formulate keyvalue to use in json file
+            //writing in the JSON label
+            let textValue = await element.textContent();
+            if(textValue == undefined || textValue == null || textValue == ''){
+                //see if the textContent is empty then check allTextContents()
+                textValue = await element.allTextContents();
+            }
+
+            if(textValue == undefined || textValue == null || textValue == ''){
+                //next try name
+                textValue = await element.getAttribute("name");
+            }
+
+            if(textValue == undefined || textValue == null || textValue == ''){
+                //if none works, use key_count for now
+                textValue = key+'_'+count
+            }
+
+            //santize textvalue to make it compatible with json key - removing all non-alphanumeric values with dash 
+            textValue = textValue.replace(/\W+/g, '_')
+            let keyvalue = pagetitle+'_'+textValue
+
+
+            if(attributes instanceof Array){
+                
+                for(let attkey in attributes){
+                    //loop through each of the given attributes and identify the locator for each.
+                    let attribute = attributes[attkey];
+                    let value = await element.getAttribute(attribute);
+                    if(value != 'undefined' && value != null && value != '' ){
+                        let selector = '';
+                        if(attribute == 'id'){
+                            selector = '#'+value
+                        }
+                        else if (attribute == 'class'){
+                            selector = '.'+value
+                        }
+                        else{
+                            selector = '//'+key+'[@'+ attribute+ '=\''+value+'\']'
+                        }
+                    
+                        //once the selector is formulated, push it to the json
+                        if(elementsJson[keyvalue] != undefined){
+                            elementsJson[keyvalue].push(selector);
+                        }
+                        else{
+                            elementsJson[keyvalue] = [selector];
+                        }
+                    }
+                }
+            }
+            else{
+                
+                let value = await element.getAttribute(attributes);
+                if(value != 'undefined' && value != null && value != '' ){
+                    //Handling for string
+                    let selector = '';
+                        if(attributes == 'id'){
+                            selector = '#'+value
+                        }
+                        else if (attributes == 'class'){
+                            selector = '.'+value
+                        }
+                        else{
+                            selector = '//'+key+'[@'+ attributes+ '=\''+value+'\']'
+                        }
+                    
+                        //once the selector is formulated, push it to the json
+                        if(elementsJson[keyvalue] != undefined){
+                            elementsJson[keyvalue].push(selector);
+                        }
+                        else{
+                            elementsJson[keyvalue] = [selector];
+                        }
+                    
+                }
+            }
+        }
+
     }
-    for(let i=0;i<links.length;i++){
-        let element=links[i]
-        let idvalue=await element.getAttribute("id")
-        let namevalue=await element.getAttribute("name")
-    
-    if(idvalue!=null & namevalue!=null){
-        dataobj[pagetitle+"_"+namevalue]=["#"+idvalue,"."+namevalue]
-    }
-    else if(idvalue!=null){
-        dataobj[pagetitle+"_"+idvalue]="#"+idvalue   
-    }
-    else if(namevalue!=null){
-        dataobj[pagetitle+"_"+namevalue]="."+namevalue   
-    }
-    }
-    
-    fs.writeFile(filepath,JSON.stringify(dataobj,null,1),err =>{
+//    let jsonString = JSON.stringify(elementsJson);
+    let filepath=testdatapath + pagetitle +'_page_' + Date.now()+'.json';
+    fs.writeFile(filepath,JSON.stringify(elementsJson,null,1),err =>{
         if(err){
             console.log(err)
         }
@@ -42,6 +125,4 @@ test('@FindElements',async ({ page }) => {
             console.log("File Written Successfully !")
         }
     })
-}
- }
-)
+})
